bd2e797d02c34a498fbce2df03846fb8f0f092e0
Nikita Goncharuk <nikitago2000@gmail.com>
2021-03-15 11:48:55 +0700

    [UPDATE]

--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
 from setuptools import setup
 
 setup(
-    entry_points={'console_scripts': ['hate-2020-formatter=hooks.hate_2020_formatter:main']}
+    entry_points={'console_scripts': ['hate-2020-formatter=hate_2020_formatter:main']}
 )
